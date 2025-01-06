85a6b131d12b14b61b257c2044c092d7888a600a
Manuel <manuelrosen@t-online.de>
2025-01-06 13:09:56 +0000

    old_excercises

--- lab1/client.py
+++ lab1/client.py
@@ -8,7 +8,7 @@ import const_cs
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((const_cs.HOST, const_cs.PORT))  # connect to server (block until accepted)
 
-s.send("Hello, world".encode('utf-8'))  # send encoded string as data
+s.send("Hello, world".encode('utf-8'))  # send encoded string as data test
 data = s.recv(1024)  # receive the response
 
 print(data.decode('utf-8'))  # print the result
